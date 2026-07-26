a58f7969bee89f6ec880260817936ef9a6b5491b
Parth Sharma <parthsharma1362006@gmail.com>
2026-07-26 22:09:40 +0530

    Fix User model import casing in auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/User.model.js";
+import { User } from "../models/user.model.js";
 
 const verifyJWT = async (req, res, next) => {
   try {
